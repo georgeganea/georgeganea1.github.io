1e35f1c5034a00f6c93cd9bc67890d2c3baa9fe0
georgeganea <georgeganea@gmail.com>
2023-02-27 19:36:00 +0200

    add first version

---
+++ index.js
@@ -0,0 +1,21 @@
+
+
+const redirectFunction = (e) => {
+  const params = new Proxy(new URLSearchParams(window.location.search), {
+    get: (searchParams, prop) => searchParams.get(prop),
+  });
+  // Get the value of "some_key" in eg "https://example.com/?redirect=facebook.com"
+  let value = params.redirect; // "facebook.com"
+
+  console.log('value', value)
+
+  chrome.tabs.update(
+    updateProperties = {url:  value}
+  );
+}
+
+
+const redirecto = document.getElementById("override-button")
+
+redirecto.addEventListener("click", redirectFunction);
+
